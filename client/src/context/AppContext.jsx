e06accc68d9da201141e14c9cbe6819691eca9a8
Terence Holmes <piece4ulmind@gmail.com>
2025-10-21 14:30:06 -0400

    Navbar created

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,20 @@
+import { createContext, useContext, useState} from "react";
+import { useNavigate } from "react-router-dom";
+
+export const AppContext = createContext();
+
+export const AppContextProvider = ({children}) => {
+    const navigate = useNavigate();
+    const [user, setUser] = useState(null)
+    const [isSeller, setisSeller] = useState(false)
+
+    const value = {navigate, user, setUser, isSeller, setisSeller}
+    
+    return <AppContext.Provider value={value}>
+        {children}
+    </AppContext.Provider>
+}
+
+export const useAppContext = () => {
+    return useContext(AppContext)
+}
