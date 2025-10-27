6661342739a2fc564090a09e1ee82cbe1c8a83c2
Terence Holmes <piece4ulmind@gmail.com>
2025-10-27 13:07:32 -0400

    Navbar set up Pt3

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -12,9 +12,11 @@ const Navbar = () => {
      }
 
   return (
-    <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
+    // Navigation line 
+    <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 
+                    py-4 border-b border-gray-200 bg-white relative transition-all">
 
-            <NavLink to='/'>
+            <NavLink to='/' onClick={()=> setOpen(false)}>
                 <img className="h-9" src="./src/assets/logo.svg" alt="logo" />
             </NavLink>
 
@@ -34,16 +36,36 @@ const Navbar = () => {
                     </svg>
                 </div>
 
-                <div className="relative cursor-pointer">
-                    <svg width="18" height="18" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M.583.583h2.333l1.564 7.81a1.17 1.17 0 0 0 1.166.94h5.67a1.17 1.17 0 0 0 1.167-.94l.933-4.893H3.5m2.333 8.75a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0m6.417 0a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0" stroke="#615fff" stroke-linecap="round" stroke-linejoin="round" />
+                <div onClick={()=> navigate("/cart")} className="relative cursor-pointer">
+                    
+                    {/*Shopping cart icon*/}
+                    <svg 
+                      width="18" height="18" viewBox="0 0 14 14" fill="none"
+                      xmlns="http://www.w3.org/2000/svg"> <path d="M.583.583h2.333l1.564 
+                      7.81a1.17 1.17 0 0 0 1.166.94h5.67a1.17 1.17 0 0 0 
+                      1.167-.94l.933-4.893H3.5m2.333 8.75a.583.583 0 1 
+                      1-1.167 0 .583.583 0 0 1 1.167 0m6.417 0a.583.583 0 1 1-1.167 0 
+                      .583.583 0 0 1 1.167 0" stroke="#010013ff" stroke-linecap="round"
+                      stroke-linejoin="round" />
                     </svg>
+
+                    {/*Number of items in shopping cart */}
                     <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">3</button>
                 </div>
 
-                <button className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
+                {!user ? ( <button onClick={()=> setShowUserLogin(true)} className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
                     Login
-                </button>
+                </button> )
+                :
+                (
+                  <div className='relative group'>
+                    <img src={"./src/assets/profile-user.svg"} className='w-10' alt="" />
+                    <ul className='hidden group-hover:block absolute top-10 right-0 bg white shadow border-gray-200 py-2.5 w-30 rounded-md text-sm z-40'>
+                        <li onClick={()=> navigate("my-orders")} className='p-1.5 pl-3 hover:bg-primary/10 cursor-pointer'>My Orders</li>
+                        <li onClick={logout} className='p-1.5 pl-3 hover:bg-primary/10 cursor-pointer'>Logout</li>
+                    </ul>
+                  </div>
+                )}
             </div>
 
             <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="sm:hidden">
