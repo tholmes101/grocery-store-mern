2e031e99e58b1f1bc4f0d2aad05006846e8da960
Terence Holmes <piece4ulmind@gmail.com>
2025-10-28 21:22:25 -0400

    Banner added + more images

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
 
 const App = () => {
   return (
     <div>
        <Navbar />
+        <div className='px-6 md:px-16 lg:px-24 xl:px-32'>
+          <Routes>
+             <Route path='/' element={<Home/>}  />
+          </Routes>
+        </div>
     </div>
   )
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
 import { useAppContext } from '../context/AppContext'
+import { assets } from '../assets/assets'
 
 const Navbar = () => {
      const [open, setOpen] = React.useState(false)
@@ -17,7 +18,7 @@ const Navbar = () => {
                     py-4 border-b border-gray-200 bg-white relative transition-all">
 
             <NavLink to='/' onClick={()=> setOpen(false)}>
-                <img className="h-9" src="./src/assets/logo.svg" alt="logo" />
+                <img className="h-9" src={assets.logo} alt="logo" />
             </NavLink>
 
             {/* Desktop Menu */}
@@ -59,7 +60,7 @@ const Navbar = () => {
                 :
                 (
                   <div className='relative group'>
-                    <img src={"./src/assets/profile-user.svg"} className='w-10' alt="" />
+                    <img src={assets.profile_user} className='w-10' alt="" />
                     <ul className='hidden group-hover:block absolute top-10 right-0 bg white shadow border-gray-200 py-2.5 w-30 rounded-md text-sm z-40'>
                         <li onClick={()=> navigate("my-orders")} className='p-1.5 pl-3 hover:bg-primary/10 cursor-pointer'>My Orders</li>
                         <li onClick={logout} className='p-1.5 pl-3 hover:bg-primary/10 cursor-pointer'>Logout</li>
