ea3730891ff85a0835134ee6b4145c57034ee57f
Terence Holmes <piece4ulmind@gmail.com>
2025-11-30 21:38:40 -0500

    Product details & related products complete

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -4,13 +4,16 @@ import { useAppContext } from "../context/AppContext";
 
 
 const ProductCard = ({product}) => {
-    const [count, setCount] = React.useState(0);
+   
     const {currency, addToCart, removeFromCart, cartItems, navigate} = useAppContext()
 
     
 
     return product && (
-        <div className="border border-gray-500/20 rounded-md md:px-4 px-3 
+        <div onClick={()=> 
+        {navigate(`/products/${product.category.toLowerCase()}/${product._id}`); 
+        scrollTo(0,0)}} 
+        className="border border-gray-500/20 rounded-md md:px-4 px-3 
         py-2 bg-white min-w-56 max-w-56 w-full">
             <div className="group cursor-pointer flex items-center 
             justify-center px-2">
