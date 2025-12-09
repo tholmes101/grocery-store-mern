093d6bdfc8e98618b3140eaecf86c58be2b28f1d
Terence Holmes <piece4ulmind@gmail.com>
2025-12-09 01:44:19 -0500

    Shopping cart complete

--- client/src/components/MainBanner.jsx
+++ client/src/components/MainBanner.jsx
@@ -9,7 +9,7 @@ const MainBanner = () => {
       <img src={assets.main_banner_bg_sm} alt="banner" className='w-full md:hidden' />
 
       {/*Heading and styles to add inside the banner */}
-      <div className='absolute inset-0 flex flex-col items-cent md:items-start justify-end
+      <div className='absolute inset-0 flex flex-col items-center md:items-start justify-end
         md:justify-center pb-24 md:pb-0 px-4 md:pl-18 lg:pl-24'>
         <h1 className='text-3xl md:text-4xl lg:text-5xl font-bold text-center md:text-left 
           max-w-72 md:max-w-80 lg:max-w-105 leading-tight lg:leading-15'
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,7 +5,8 @@ import { assets } from '../assets/assets'
 
 const Navbar = () => {
      const [open, setOpen] = React.useState(false)
-     const {user, setUser, setShowUserLogin, navigate, setSearchQuery, searchQuery}
+     const {user, setUser, setShowUserLogin, navigate, setSearchQuery, searchQuery,
+     getCartCount}
       = useAppContext()
 
      const logout = async () => {
@@ -41,6 +42,7 @@ const Navbar = () => {
                     className="py-1.5 w-full bg-transparent outline-none 
                     placeholder-gray-500" type="text" placeholder="Search products" />
                     
+                    {/*Search icon*/}
                     <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.836 10.615 15 14.695" stroke="#7A7B7D" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" />
                         <path clip-rule="evenodd" d="M9.141 11.738c2.729-1.136 4.001-4.224 2.841-6.898S7.67.921 4.942 2.057C2.211 3.193.94 6.281 2.1 8.955s4.312 3.92 7.041 2.783" stroke="#7A7B7D" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" />
@@ -48,7 +50,6 @@ const Navbar = () => {
                 </div>
 
                 <div onClick={()=> navigate("/cart")} className="relative cursor-pointer">
-                    
                     {/*Shopping cart icon*/}
                     <svg 
                       width="18" height="18" viewBox="0 0 14 14" fill="none"
@@ -61,7 +62,8 @@ const Navbar = () => {
                     </svg>
 
                     {/*Number of items in shopping cart */}
-                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">3</button>
+                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary 
+                    w-[18px] h-[18px] rounded-full">{getCartCount()}</button>
                 </div>
 
                 {!user ? ( <button onClick={()=> setShowUserLogin(true)} 
@@ -83,14 +85,35 @@ const Navbar = () => {
                 )}
             </div>
 
-            <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="sm:hidden">
+            <div className='flex items-center gap-6 sm:hidden'>
+                <div onClick={()=> navigate("/cart")} className="relative cursor-pointer">
+                    {/*Shopping cart icon*/}
+                    <svg 
+                      width="18" height="18" viewBox="0 0 14 14" fill="none"
+                      xmlns="http://www.w3.org/2000/svg"> <path d="M.583.583h2.333l1.564 
+                      7.81a1.17 1.17 0 0 0 1.166.94h5.67a1.17 1.17 0 0 0 
+                      1.167-.94l.933-4.893H3.5m2.333 8.75a.583.583 0 1 
+                      1-1.167 0 .583.583 0 0 1 1.167 0m6.417 0a.583.583 0 1 1-1.167 0 
+                      .583.583 0 0 1 1.167 0" stroke="#010013ff" stroke-linecap="round"
+                      stroke-linejoin="round" />
+                    </svg>
+
+                    {/*Number of items in shopping cart */}
+                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary 
+                    w-[18px] h-[18px] rounded-full">{getCartCount()}</button>
+                </div>
+               <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" 
+               className="">
                 {/* Menu Icon SVG */}
-                <svg width="21" height="15" viewBox="0 0 21 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <rect width="21" height="1.5" rx=".75" fill="#426287" />
-                    <rect x="8" y="6" width="13" height="1.5" rx=".75" fill="#426287" />
-                    <rect x="6" y="13" width="15" height="1.5" rx=".75" fill="#426287" />
-                </svg>
-            </button>
+                    <svg width="21" height="15" viewBox="0 0 21 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <rect width="21" height="1.5" rx=".75" fill="#426287" />
+                        <rect x="8" y="6" width="13" height="1.5" rx=".75" fill="#426287" />
+                        <rect x="6" y="13" width="15" height="1.5" rx=".75" fill="#426287" />
+                    </svg>
+                </button>            
+            </div>
+
+            
 
             {/* Mobile Menu */}
           { open && (
@@ -106,7 +129,8 @@ const Navbar = () => {
                     <button onClick={()=> {
                         setOpen(false);
                         setShowUserLogin(true);
-                    }} className="cursor-pointer px-6 py-2 mt-2 bg-primary hover:bg-primary-dull transition text-white rounded-full text-sm">
+                    }} className="cursor-pointer px-6 py-2 mt-2 bg-primary hover:bg-primary-dull transition 
+                    text-white rounded-full text-sm">
                     Login
                 </button>
                 ) : (
