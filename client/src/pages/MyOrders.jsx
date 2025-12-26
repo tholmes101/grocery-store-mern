a9b2752d2a8cf0c1c1b62af4f2a64b5d14377b4d
tholmes101 <piece4ulmind@gmail.com>
2025-12-26 10:28:56 -0500

    My Orders Page 10% complete

---
+++ client/src/pages/MyOrders.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const MyOrders = () => {
+  
+  return (
+    <div className='mt-16 pb-16'>
+      <div className='flex flex-col items-end w-max mb-8'>
+        <p className='text-2xl font-medium uppercase'>My Orders</p>
+        <div className='w-16 h-0.5 bg-primary rounded-full'></div>
+      </div>
+    </div>
+  )
+}
+
+export default MyOrders
