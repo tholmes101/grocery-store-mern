f1712f974a83a88dd18fc5ea09cb2d94e4a2fc53
tholmes101 <piece4ulmind@gmail.com>
2026-02-04 23:51:36 -0500

    Update added

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ await connectDB()
 await connectCloudinary()
 
 //Allow multiple origins
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173', 'https://grocery-store-mern-ochre.vercel.app']
 
 // Stripe route endpoint - to verify the order placed
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
